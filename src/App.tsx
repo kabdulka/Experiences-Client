6936870e6454a852bcb05b4d8f0b50478cbda38b
kabdulka <kenabdulka@gmail.com>
2024-01-09 23:57:35 -0800

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+// import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <div>Experiences Client</div>
+  )
+}
+
+export default App
