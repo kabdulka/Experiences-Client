c1737ed758e6d3acea2a24ec79759c75e4b52340
kabdulka <kenabdulka@gmail.com>
2024-01-10 20:56:31 -0800

    add post functionality and styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,35 @@
 // import { useState } from 'react'
 
-import './App.css'
+import './App.scss'
+import { Container, Typography, Grow, Grid } from '@mui/material';
+import experiences from './images/experiences.jpeg'
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
+// import { styled } from '@mui/material/styles';
+// import { styled } from '@mui/system';
+import { StyledAppBar, imgStyle, headingStyle } from './styles';
 
-function App() {
-  // const [count, setCount] = useState(0)
+const App: React.FC = () => {
 
   return (
-    <div>Experiences Client</div>
+    <Container maxWidth="lg">
+      <StyledAppBar position='static' color="inherit" >
+        <Typography style={headingStyle} variant='h2' align='center'> Memories </Typography>
+        <img style={imgStyle} src={experiences} alt="Experiences" height="60"/>
+      </StyledAppBar>
+      <Grow in>
+        <Container>
+          <Grid container justifyContent="space-between" alignItems="stretch" spacing={3}> 
+            <Grid item xs={12} sm={7}>  
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>  
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   )
 }
 
